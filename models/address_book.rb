fb49dd61c105848137b8c67cf801ff3ba3bc12cf
Pablo Palomo 3 <pablopalomo3@gmail.com>
2018-04-30 05:07:59 +0000

    assignment 2: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -30,4 +30,14 @@ class AddressBook
    
   end
   
+  def remove_entry(name, phone_number, email)
+     delete_entry = nil
+      entries.each do |entry|
+        if name == entry.name && phone_number == entry.phone_number && email == entry.email
+          delete_entry = entry
+        end
+      end
+      entries.delete(delete_entry)
+  end
+  
 end
